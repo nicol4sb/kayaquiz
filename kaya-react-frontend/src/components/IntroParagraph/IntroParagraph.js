3832e94ed7fd50c08dbf8356d39479b15618436b
nic <nicolasb@gmail.com>
2024-04-15 17:57:28 +0200

    Better GDP diagram

--- kaya-react-frontend/src/components/IntroParagraph/IntroParagraph.js
+++ kaya-react-frontend/src/components/IntroParagraph/IntroParagraph.js
@@ -6,10 +6,10 @@ function IntroParagraph() {
       <div className='intro-paragraph'>
         <p className='title'>How does our future look like?</p>
         <p>
-          In order to limit global warming to 1.5°C or 2°C by 2100, we will need to significantly reduce global CO2 emissions by 2050.
+          In order to limit global warming to 1.5°C or 2°C by 2100, we will need to significantly reduce global CO2 emissions by 2050 (and continue to reduce after that).
         </p>
         <p>
-          You will be asked 3 questions to help judge if this is reachable.
+          You will be asked 3 questions that will help understand the arbitrages needed to get therex.
         </p>
       </div>
     );
--- kaya-react-frontend/src/components/Questions/Question1.js
+++ kaya-react-frontend/src/components/Questions/Question1.js
@@ -1,19 +1,16 @@
 import "./Question.css";
-import projections from"../../assets/PROJECTIONS-95-hi-lo-rec_0.png"
+import projections from "../../assets/PROJECTIONS-95-hi-lo-rec_0.png";
 function Question1() {
   return (
     <div className="question">
       <p>1. World Population?</p>
-      <p>
-        What do you believe the world population in 2050 will be? Current world
-        population is estimated at 8 billion people.
-      </p>
+      <p>What do you believe the world population in 2050 will be?</p>
+      <p>Current world population is estimated at 8 billion people.</p>
       <p>
         UN projections are for the world population to be between 9.4b and 10.1b
         in 2050
       </p>
       <img src={projections} alt="CO2" />
-
     </div>
   );
 }
