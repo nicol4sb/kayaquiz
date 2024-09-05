13c723738574fa4ca461516404b901a7254c859a
nic <nicolasb@gmail.com>
2024-09-05 11:57:58 +0200

    Adding consequences slide

--- kaya-react-frontend/src/components/FacilitatorQR/IdentitySlide.js
+++ kaya-react-frontend/src/components/FacilitatorQR/IdentitySlide.js
@@ -1,10 +1,12 @@
 import React from "react";
-import kayaIdentityImage from "../../assets/kayaIdentity.png";
+import consequences_1 from "../../assets/consequences_1.png";
+import consequences_2 from "../../assets/consequences_2.png";
 
 function IdentitySlide() {
   return (
     <div className="slide">
-      <img src={kayaIdentityImage} alt="Kaya Identity Equation" className="equation-image" />
+      <img src={consequences_1} alt="consequences_1" className="equation-image" />
+      <img src={consequences_2} alt="consequences_2" className="equation-image" />
     </div>
   );
 }
